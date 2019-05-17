41d4c0ba6ec93e86094d4fbf189b17ea679a623f
Edgar Isai <edgarisaiwr@gmail.com>
2019-05-16 22:34:41 -0500

    Add a script that translates assembly to binary, just addi instruction supported

---
+++ assembly_binary.py
@@ -0,0 +1,65 @@
+import re
+
+class Assembly:
+
+    def __init__(self):
+        self.opt = {
+        'add':'000000',
+        'addi':'001000',
+        'div':'000000',
+        'mfhi':'000000',
+        'beq':'000100',
+        'sub':'000000',
+        'j':'000010',
+        }
+
+    def clean(self, file):
+        self.file = file
+        pattern = r'[\w]+\s[$#][\d]+,?\s?[#$]?[\d]{0,},?\s?[$#]?[\d]{0,}'
+        # fetch all the instructions
+        with open(file, 'r') as original:
+            for line in original:
+                filedata = original.read()
+                instructions = re.findall(pattern, filedata)
+        # Create a new file to store the instructions
+        with open('copy.txt', 'w') as copy:
+            for instruction in instructions:
+                copy.write(instruction+'\n')
+        # Erase unwanted symbols from instructions
+        with open('copy.txt', 'r') as file:
+            data = file.read()
+            for line in data:
+                data = data.replace(',', '')
+                data = data.replace('#', '')
+                data = data.replace('$', '')
+
+        with open('copy.txt', 'w') as file:
+            file.write(data)
+
+    def binary(self, file):
+
+        def append_binary(file):
+            with open(file, 'a') as append_file:
+                start = 0
+                end = 8
+                for _ in range(4):
+                    append_file.write(binary[start:end] + '\n')
+                    start = end
+                    end += 8
+
+
+        with open(file, 'r') as file_:
+            for line in file_:
+                instruction = line.split()
+                binary = ''
+                if instruction[0] == 'addi':
+                    binary += self.opt['addi'] +\
+                    format(int(instruction[2]), '05b') +\
+                    format(int(instruction[1]), '05b') +\
+                    format(int(instruction[3]), '016b')
+                    append_binary('binary.txt')
+
+if __name__ == '__main__':
+    target = Assembly()
+    target.clean('assembly.txt')
+    target.binary('copy.txt')
